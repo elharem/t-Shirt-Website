162a43c171604a22a7d3508d47f1d92964f76de3
laghim <simo00ayoub@gmail.com>
2026-05-27 16:56:58 +0200

    fix tailwind paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
-    ],
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    './app/**/*.php',
+],
     theme: {
         extend: {
             fontFamily: {
